481fb95bf8888ce9f8352d632e10bd9e0f71348a
sdgerving <combatlms@gmail.com>
2021-09-17 12:46:06 -0500

    initialized backend and frontend

---
+++ backend/data/notes.js
@@ -0,0 +1,27 @@
+const notes = [
+  {
+    _id: "1",
+    title: "my brother the gimp",
+    content: "ever since he was a boy, he loved latex ",
+    category: "College",
+  }, 
+  {
+    _id: "2",
+    title: "sex and guns",
+    content: "how to make the most of sex and violence",
+    category: "Learning",
+  },
+  {
+    _id: "3",
+    title: "The lost art of torture",
+    content: "confession on demand done right ",
+    category: "Entertainment",
+  },
+  {
+    _id: "4",
+    title: "how to love the dead",
+    content: "how cold flesh and a bunson burner can change your life ",
+    category: "Learning",
+  }, 
+];
+ module.exports=notes;
---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const notes = require("./data/notes");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+app.get("/", (req, res) => {
+  res.send("API is running..");
+});
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+app.get("/api/notes/:id", (req, res) => {
+  const note = notes.find((n) => n._id === req.params.id);
+  //console.log(req.params)
+  res.send(note);
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`server started on PORT ${PORT}`));
